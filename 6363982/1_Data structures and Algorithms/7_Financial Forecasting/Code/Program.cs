e7f0a8be789d97efb93867bb822ead9c77afa4da
Gautam <gautamkumar200518@gmail.com>
2025-06-22 14:07:48 +0530

    Add files via upload

---
+++ 6363982/1_Data structures and Algorithms/7_Financial Forecasting/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class FinancialForecasting
+{
+    
+    static double PredictValueRecursive(double initialValue, double growthRate, int years)
+    {
+        if (years == 0)
+            return initialValue;
+
+        return PredictValueRecursive(initialValue, growthRate, years - 1) * (1 + growthRate);
+    }
+
+    static void Main()
+    {
+        double initialValue = 1000; 
+        double growthRate = 0.1;    
+        int years = 5;
+
+        double result = PredictValueRecursive(initialValue, growthRate, years);
+        Console.WriteLine($"Future value after {years} years: {result:F2}");
+        Console.ReadLine();
+    }
+}
